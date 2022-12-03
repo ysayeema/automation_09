4b57e408aec1026e1803b8a92ec1609818b53416
Shah Aditi <111628344+aditi4119@users.noreply.github.com>
2022-12-03 17:45:00 +0600

    operator

---
+++ Operators/Comparison.py
@@ -0,0 +1,11 @@
+"""
+from pip._internal.req.constructors import operators
+
+Pythn has six comparison operators
+1. =Equal
+2.>Greater than operators
+3.<less than operators
+4.<= less than or equal operators
+5.>= Greater than or equal operators
+6.!= Not equal operators """
+
---
+++ Operators/Logical.py
@@ -0,0 +1,29 @@
+
+"""Python has 3 logical operators
+1.and
+2.or
+3.not
+
+
+To check multiple conditions and returns Boolean value True and False
+"""
+
+age=20
+#and
+# print(age>10 and age<20)
+print(10<age<20)
+#or
+print(age>10 and age<20)
+#not
+print(not age>10)
+
+
+print(10==10 or 10>10 and 10>=20 or 10<=30 and 10>30 or 10==10)
+
+
+
+
+
+
+
+
